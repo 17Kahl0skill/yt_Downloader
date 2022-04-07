b3a28cf3e42ae8466625273e93782f6493784ae9
17Kahl0skill <timm.kohlhaase@t-online.de>
2022-04-07 23:53:31 +0200

    changed to classes and refactored

--- sample/app.py
+++ sample/app.py
@@ -1,8 +1,18 @@
 import logic.Downloader as Download
 
-def main():
-    Download.download_mp3("https://www.youtube.com/watch?v=2BCHVVj9AH4")
 
+def main():
+    link = "https://www.youtube.com/playlist?list=PLiFoS88a9gul5peVkX39uRC_BkuR7-QqH"
+    # Download.download_mp3("https://www.youtube.com/watch?v=2BCHVVj9AH4")
+    downl = Download.Downloader()
+
+    # downl.download_playlist(link)
+    downl.download_playlist(link)
+    downl.download_playlist("https://www.youtube.com/watch?v=iPwNeM9I1-M&list=OLAK5uy_mojkYya3VWhN4f1CNlg8urMNhYBfwrUeE")
+    downl.download_playlist("https://www.youtube.com/watch?v=Eg84Eu_Qonw&list=OLAK5uy_kUu4FarXr48s_uQqrEpwCE-uOobKic5xQ")
+    downl.download_playlist("https://www.youtube.com/watch?v=XRP9k9nlAfE&list=PLMEOLAYxGgstLEUGRx8kCF15EFBG5M95X&index=1")
+    downl.download_playlist("https://www.youtube.com/watch?v=inHbha2nyPs&list=PLMEOLAYxGgsttujEl1yBrKAdUsVBcX7U3")
+    del downl
     return
 
 
--- sample/logic/Downloader.py
+++ sample/logic/Downloader.py
@@ -1,23 +1,65 @@
+import os
 import pytube
 import ffmpeg
-dir="./data/"
-dir_aud=dir+"audio/"
-dir_vid=dir+"video/"
-video_format=".mp4"
-audio_format=".mp3"      
+import shutil
 
 
-def download_highest_quality(url_link):
-    yt= pytube.YouTube(url_link,use_oauth=False)
-    yt.streams.filter(progressive=False).order_by(attribute_name="resolution").desc().first().download(dir_vid,filename=yt.title + video_format)
-    yt.streams.get_audio_only().download(dir_aud,filename=yt.title + audio_format)
-    print("download fertig")
-    video = ffmpeg.input(dir_vid+yt.title+video_format).video
-    audio = ffmpeg.input(dir_aud+yt.title+audio_format).audio
-    ffmpeg.concat(video,audio,v=1,a=1).output(dir+yt.title+video_format).run()
+class Downloader():
+    def __init__(self,
+                 dir="data",
+                 dir_aud="audio",
+                 dir_vid="video",
+                 format_video=".mp4",
+                 format_audio=".mp3"):
+        self.__dir = "./"+dir+"/"
+        self.__dir_aud = self.__dir+dir_aud+"/"
+        self.__dir_vid = self.__dir+dir_vid+"/"
+        self.__format_video = format_video
+        self.__format_audio = format_audio
 
+    def download_playlist(self, url_link, is_mp3=True):
+        pl = pytube.Playlist(url=url_link)
+        for vid in pl:
+            print(vid)
+            if is_mp3 is True:
+                self.download_mp3(vid)
+            else:
+                self.download_highest_quality(vid)
 
-def download_mp3(url_link):
-    yt= pytube.YouTube(url_link,use_oauth=False)
-    yt.streams.get_audio_only().download(dir,filename=yt.title+audio_format)
-    print("download fertig")
+    def download_highest_quality(self, url_link):
+        yt = pytube.YouTube(url_link, use_oauth=False)
+
+        # video sortierung und download
+        yt_vid = yt.streams.filter(progressive=False)
+        yt_vid = yt_vid.order_by(attribute_name="resolution").desc()
+        yt_vid = yt_vid.first().download(self.__dir_vid,
+                                         filename=yt.title +
+                                         self.__format_video)
+
+        # audio sortierung und bester qualitäts download
+        yt_aud = yt.streams.get_audio_only()
+        yt_aud = yt_aud.download(self.__dir_aud,
+                                 filename=yt.title + self.__format_audio)
+        print("download fertig")
+
+        # editing
+        video = ffmpeg.input(self.__dir_vid+yt.title+self.__format_video).video
+        audio = ffmpeg.input(self.__dir_aud+yt.title+self.__format_audio).audio
+        edit = ffmpeg.concat(video, audio, v=1, a=1)
+        edit.output(self.__dir+yt.title+self.__format_video).run()
+
+    def download_mp3(self, url_link):
+        yt = pytube.YouTube(url_link, use_oauth=False)
+        yt.streams.get_audio_only().download(self.__dir,
+                                             filename=yt.title +
+                                             self.__format_audio)
+        print("download fertig")
+
+    def delete_temp_data(self):
+        if os.path.exists(self.__dir_vid):
+            shutil.rmtree(self.__dir_vid)
+        if os.path.exists(self.__dir_aud):
+            shutil.rmtree(self.__dir_aud)
+
+    def __del__(self):
+        self.delete_temp_data()
